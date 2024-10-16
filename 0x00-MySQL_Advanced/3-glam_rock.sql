1a771c52e9359bc95cbbd6b0e5c4ab35f4ed4986
mumugisha <prudent.nkubito@gmail.com>
2024-10-17 01:02:21 +0300

    msql Advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
+--  Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, 
        (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
 FROM metal_bands
