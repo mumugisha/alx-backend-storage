59f6b46537a9e1f4604b1e0a24a050141caab349
mumugisha <prudent.nkubito@gmail.com>
2024-10-17 00:45:48 +0300

    msql Advance

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans 
+SELECT ORIGIN, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY ORIGIN
+    ORDER BY nb_fans DESC;
