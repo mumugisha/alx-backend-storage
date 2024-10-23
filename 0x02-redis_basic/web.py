4488fc4d33920eed5c0770e44407809241244700
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-10-24 01:30:12 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -38,6 +38,7 @@ def get_page(url: str) -> str:
     """
     Fetch the HTML content of a given URL.
     """
-    resp = requests.get('http://slowwly.robertomurray.co.uk/delay/10000/'
-        'url/https://www.example.com')
+    resp = requests.get(
+        'http://slowwly.robertomurray.co.uk/delay/10000/url/https://www.example.com'
+    )
     return resp.text
