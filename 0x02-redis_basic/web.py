06d419a8fa2b1f2ecda838460e2bbdce46ff1e6d
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-10-25 22:38:57 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,49 +1,43 @@
 #!/usr/bin/env python3
-"""
-Implementing an expiring web cache and tracker
-"""
-import redis
+"""Redis web server"""
+
 import requests
-from functools import wraps
+import redis
+import time
 
-store = redis.Redis()
-
-
-def count_url_access(method):
-    """
-    Track how many times a particular URL was accessed
-    and cache the result for 10 seconds.
-    """
-    @wraps(method)
-    def wrapper(url: str):
-        result_cache_key = "cached:" + url
-        count_cache_key = "count:" + url
-
-        result_cache_data = store.get(result_cache_key)
-
-        if result_cache_data:
-            return result_cache_data.decode("utf-8")
-
-        store.incr(count_cache_key)
-
-        html = method(url)
-
-        store.set(result_cache_key, html)
-        store.expire(result_cache_key, 10)
-        return html
-    return wrapper
+r = redis.Redis(host="localhost", port=6379, db=0)
 
 
-@count_url_access
 def get_page(url: str) -> str:
-    """
-    Fetch the HTML content of a given URL.
-    For testing, return just the status code to pass the test.
-    """
-    resp = requests.get(url)
-    return str(resp.status_code)
+    """Get page from url and keeps a count record of visiting times"""
+    cache_key = f"page:{url}"
+    count_key = f"count:{url}"
+
+    cached_page = r.get(cache_key)
+
+    if cached_page:
+        print("Returning cached page...")
+        return cached_page.decode("utf-8")
+
+    print("Fetching new page...")
+    response = requests.get(url)
+
+    r.setex(cache_key, 10, response.text)
+
+    r.incr(count_key)
+
+    return response.text
 
 
 if __name__ == "__main__":
-    url = "http://google.com"
+    url = "http://slowwly.robertomurray.co.uk"
+
+    print(get_page(url))
+
+    time.sleep(2)
+
+    print(get_page(url))
+
+    time.sleep(11)
+
     print(get_page(url))
